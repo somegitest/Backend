1c93236a194bfe1bfcc1cdbe12985578f6b44028
8786795@gmail.com <8786795@gmail.com>
2016-08-22 17:14:05 +0300

    Initial commit

---
+++ App_Start/WebApiConfig.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+using System.Net.Http.Headers;
+using System.Web.Http;
+using System.Web.Http.Dependencies;
+using System.Web.Routing;
+using Microsoft.Owin.Security.OAuth;
+using Microsoft.Practices.Unity;
+using Newtonsoft.Json;
+using SecurityScanner.Data;
+using RouteCollectionExtensions = System.Web.Mvc.RouteCollectionExtensions;
+using SecurityScanner.Core.Interfaces;
+using SecurityScanner.Core;
+
+namespace SecurityScannerAPI
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+            // Configure Web API to use only bearer token authentication.
+            config.SuppressDefaultHostAuthentication();
+            config.Filters.Add(new HostAuthenticationFilter(OAuthDefaults.AuthenticationType));
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+   
+            RouteTable.Routes.MapHttpRoute("WithActionApi", "api/{controller}/{action}");
+            RouteTable.Routes.MapHttpRoute("DefaultApi", "api/{controller}/{id}",
+                new { action = "DefaultAction", id = RouteParameter.Optional });
+
+
+            var jsonFormatter = new JsonMediaTypeFormatter();
+            //optional: set serializer settings here
+            config.Services.Replace(typeof(IContentNegotiator), new JsonContentNegotiator(jsonFormatter));
+
+
+            var json = config.Formatters.JsonFormatter;
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
+
+
+            config.Formatters.JsonFormatter.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+            //inject data dependencies
+            var container = new UnityContainer();
+            container.RegisterType<IRepository, AzureEfRepository>(new HierarchicalLifetimeManager());
+#if DEBUG
+            container.RegisterType<IMessenger, TestMessenger>(new HierarchicalLifetimeManager());
+#else
+            container.RegisterType<IMessenger, ReportMessenger>(new HierarchicalLifetimeManager());
+#endif
+            config.DependencyResolver = new UnityResolver(container);
+        }
+    }
+
+
+    public class JsonContentNegotiator : IContentNegotiator
+    {
+        private readonly JsonMediaTypeFormatter _jsonFormatter;
+
+        public JsonContentNegotiator(JsonMediaTypeFormatter formatter)
+        {
+            _jsonFormatter = formatter;
+        }
+
+        public ContentNegotiationResult Negotiate(
+                Type type,
+                HttpRequestMessage request,
+                IEnumerable<MediaTypeFormatter> formatters)
+        {
+            return new ContentNegotiationResult(
+                _jsonFormatter,
+                new MediaTypeHeaderValue("application/json"));
+        }
+    }
+
+    public class UnityResolver : IDependencyResolver
+    {
+        protected IUnityContainer container;
+
+        public UnityResolver(IUnityContainer container)
+        {
+            if (container == null)
+            {
+                throw new ArgumentNullException("container");
+            }
+            this.container = container;
+        }
+
+        public object GetService(Type serviceType)
+        {
+            try
+            {
+                return container.Resolve(serviceType);
+            }
+            catch (ResolutionFailedException)
+            {
+                return null;
+            }
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            try
+            {
+                return container.ResolveAll(serviceType);
+            }
+            catch (ResolutionFailedException)
+            {
+                return new List<object>();
+            }
+        }
+
+        public IDependencyScope BeginScope()
+        {
+            var child = container.CreateChildContainer();
+            return new UnityResolver(child);
+        }
+
+        public void Dispose()
+        {
+            container.Dispose();
+        }
+    }
+
+
+}
---
+++ Controllers/ScanController.cs
@@ -0,0 +1,87 @@
+﻿using System.Net;
+using System.Reflection;
+using System.Threading;
+using System.Web.Http;
+using SecurityScanner.Core;
+using SecurityScanner.Core.Interfaces;
+using SecurityScanner.Data;
+using SecurityScanner.Logging;
+
+namespace SecurityScannerAPI.Controllers
+{
+    public class ScanController : ApiController
+    {
+        private const string SecurityToken = "test";
+
+        private readonly IOpenVasWorkflow _workFlow;
+
+        private readonly IRepository _repository;
+        private readonly IMessenger _messenger;
+
+        private readonly string _targetOpenVasHost;
+
+   
+
+        public ScanController(IRepository repository, IMessenger messenger)
+        {
+            _repository = repository;
+            _messenger = messenger;
+            var targetOpenVasHost = this._repository.GetTargetOpenVasHostAddress();
+            _workFlow = new OpenVasWorkflow(repository, targetOpenVasHost);
+           
+        }
+
+        /// <summary>
+        /// Invoke this method to see if there are any completed scans. If there are any completed scans, the report will be created in the default format (HTML) and send to the customer's emailAddress.
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public bool RunReportsForCompletedTasks(string token)
+        {
+            //TODO make this better
+            if (token != SecurityToken)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+
+            LogHelper.LogInfo(this, MethodBase.GetCurrentMethod().Name, this.RequestContext.Url.Request.RequestUri.OriginalString);
+            var backgroundWorkerThread = new Thread(RunReportsForCompletedTasksStart);
+            backgroundWorkerThread.Start();
+
+            return true;
+        }
+
+        /// <summary>
+        /// Invoke this method to check if any scheduled tasks must be run
+        /// </summary>
+        /// <param name="token"></param>
+        /// <returns></returns>
+        [HttpGet]
+        public bool CheckScheduleTasksToRun(string token)
+        {
+            //TODO make this better
+            if (token != "test")
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            LogHelper.LogInfo(this, MethodBase.GetCurrentMethod().Name, this.RequestContext.Url.Request.RequestUri.OriginalString);
+            var backgroundWorkerThread = new Thread(CheckScheduleTasksToRunStart);
+            backgroundWorkerThread.Start();
+            return true;
+        }
+
+        private void RunReportsForCompletedTasksStart()
+        {
+            _workFlow.RunReportsForCompletedTasks();  
+        }
+
+        private void CheckScheduleTasksToRunStart()
+        {
+            var scanOrderIDList = _repository.GetScanOrdersToRunTasksOnSchedule();
+
+            foreach (var scanOrder in scanOrderIDList)
+            {
+                ScanOrderOccurence occurence = _repository.CreateScanOrderOccurence(System.DateTime.UtcNow, scanOrder.ScanOrderID);
+                _workFlow.StartScan(scanOrder, occurence.ScanOrderOccurenceID);
+                //Customers can have as many scan orders as they want. For right now, do not limit Customer "A" from purchashing N number of "Daily" scans.
+                _messenger.SendInternalEmail("Scheduled Scan Starting", string.Format("Schedule Scan starting for IP Address {0}", scanOrder.TargetIPAddress));
+            }
+        }
+    }
+}
